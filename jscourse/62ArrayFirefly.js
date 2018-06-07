d0cab8898be0558b73d628e055e2c2793c0a6b02
sreeram.h4 <29130266+sreeramh4@users.noreply.github.com>
2018-06-07 22:15:27 +0530

    Arrays

---
+++ jscourse/62ArrayFirefly.js
@@ -0,0 +1,11 @@
+62 Array Firefly crew
+var captain = "Mal";
+var second = "Zoe";
+var pilot = "Wash";
+var companion = "Inara";
+var mercenary = "Jayne";
+var mechanic = "Kaylee";
+
+// your code goes here
+var crew = [captain, second, pilot, companion, mercenary, mechanic];
+console.log(crew);
