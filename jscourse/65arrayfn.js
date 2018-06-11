80e8783351e82f795e8aeb515c7b7fb778a06f43
sreeram.h4 <29130266+sreeramh4@users.noreply.github.com>
2018-06-11 17:54:14 +0530

    Explore array methods splice(), push() and inline functions

---
+++ jscourse/65arrayfn.js
@@ -0,0 +1,17 @@
+/*
+ * Programming Quiz: Quidditch Cup (6-5)
+ */
+
+// your code goes here
+
+function hasEnoughPlayers(team){
+    if(team.length>=7){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+var team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
+console.log(hasEnoughPlayers(team));
